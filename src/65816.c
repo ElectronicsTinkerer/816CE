0d6bdbc638de5341c9b786f56a7df59618534451
Electronics Tinkerer <electronicstinkerer@gmail.com>
2022-01-01 23:08:41 -0500

    Implement IRQ, NMI, BRK, COP

--- src/65816.c
+++ src/65816.c
@@ -22,7 +22,7 @@ void resetCPU(CPU *cpu)
 
     // Internal use only, tell the sim that the CPU just reset
     cpu->P.RST = 1;
-    // Somehow need to insert a jump to the vector into the IR
+    cpu->cycles = 0;
 }
 
 /**
@@ -40,15 +40,182 @@ void stepCPU(CPU *cpu, int16_t *mem)
     }
 
     // Fetch, decode, execute instruction
+    switch (mem[cpu->PC])
+    {
+        case 0x00: // BRK
+
+            // Push PC and SR
+            if (!cpu->P.E)
+            {
+                _stackCPU_pushByte(cpu, mem, cpu->PBR);
+            }
+            cpu->PC += 2;
+            _stackCPU_pushWord(cpu, mem, cpu->PC);
+
+            if (cpu->P.E)
+            {
+                _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu) | 0x10); // B gets set on the stack
+            }
+            else
+            {
+                _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu));
+            }
+
+            // Jump to ISR
+            if (cpu->P.E)
+            {
+                cpu->PC = mem[CPU_VEC_EMU_IRQ];
+                cpu->PC |= mem[CPU_VEC_EMU_IRQ + 1] << 8;
+                cpu->cycles += 7;
+            }
+            else
+            {
+                cpu->PC = mem[CPU_VEC_NATIVE_BRK];
+                cpu->PC |= mem[CPU_VEC_NATIVE_BRK + 1] << 8;
+                cpu->PBR = 0;
+                cpu->cycles += 8;
+            }
+
+            cpu->P.D = 0; // Binary mode (65C02)
+            cpu->P.I = 1;
+            break;
+
+        case 0x02: // COP
+
+            // Push PC and SR
+            if (!cpu->P.E)
+            {
+                _stackCPU_pushByte(cpu, mem, cpu->PBR);
+            }
+            cpu->PC += 2;
+            _stackCPU_pushWord(cpu, mem, cpu->PC);
+
+            if (cpu->P.E)
+            {
+                _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu) & 0xef); // ??? Unknown: the state of the B flag in ISR for COP (assumed to be 0)
+            }
+            else
+            {
+                _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu));
+            }
+
+            // Jump to ISR
+            if (cpu->P.E)
+            {
+                cpu->PC = mem[CPU_VEC_EMU_COP];
+                cpu->PC |= mem[CPU_VEC_EMU_COP + 1] << 8;
+                cpu->cycles += 7;
+            }
+            else
+            {
+                cpu->PC = mem[CPU_VEC_NATIVE_COP];
+                cpu->PC |= mem[CPU_VEC_NATIVE_COP + 1] << 8;
+                cpu->PBR = 0;
+                cpu->cycles += 8;
+            }
+
+            cpu->P.D = 0; // Binary mode (65C02)
+            cpu->P.I = 1;
+            break;
+        case 0x42: // WDM
+            cpu->PC += 2;
+            cpu->cycles += 2; //????
+            break;
+        case 0xea: // NOP
+            cpu->PC += 1;
+            cpu->cycles += 2;
+            break;
+        case 0xeb: // XBA
+            cpu->C = ( (cpu->C << 8) | ( (cpu->C >> 8) & 0xff ) ) & 0xffff;
+            cpu->P.N = cpu->C & 0x80 ? 1 : 0;
+            cpu->P.Z = cpu->C & 0xff ? 0 : 1;
+            cpu->PC += 1;
+            cpu->cycles += 3;
+            break;
+        default:
+            printf("Unknown opcode: %d\n", mem[cpu->PC]);
+            break;
+    }
 
 
     // Handle any interrupts that are pending
     if (cpu->P.NMI)
     {
+        cpu->P.NMI = 0;
+
+        // Push PC and SR
+        if (!cpu->P.E)
+        {
+            _stackCPU_pushByte(cpu, mem, cpu->PBR);
+        }
+        _stackCPU_pushWord(cpu, mem, cpu->PC);
+
+        if (cpu->P.E)
+        {
+            _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu) & 0xef); // B gets reset on the stack
+        }
+        else
+        {
+            _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu));
+        }
+
+        // Jump to ISR
+        if (cpu->P.E)
+        {
+            cpu->PC = mem[CPU_VEC_EMU_NMI];
+            cpu->PC |= mem[CPU_VEC_EMU_NMI + 1] << 8;
+            cpu->cycles += 7;
+        }
+        else
+        {
+            cpu->PC = mem[CPU_VEC_NATIVE_NMI];
+            cpu->PC |= mem[CPU_VEC_NATIVE_NMI + 1] << 8;
+            cpu->PBR = 0;
+            cpu->cycles += 8;
+        }
+
+        cpu->P.D = 0; // Binary mode (65C02)
+        cpu->P.I = 1;
+
         return;
     }
-    if (cpu->P.IRQ)
+    if (cpu->P.IRQ && !cpu->P.I)
     {
+        cpu->P.IRQ = 0;
+
+        // Push PC and SR
+        if (!cpu->P.E)
+        {
+            _stackCPU_pushByte(cpu, mem, cpu->PBR);
+        }
+        _stackCPU_pushWord(cpu, mem, cpu->PC);
+
+        if (cpu->P.E)
+        {
+            _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu) & 0xef); // B gets reset on the stack
+        }
+        else
+        {
+            _stackCPU_pushByte(cpu, mem, CPU_GET_SR(cpu));
+        }
+        // Jump to ISR
+        if (cpu->P.E)
+        {
+            cpu->PC = mem[CPU_VEC_EMU_IRQ];
+            cpu->PC |= mem[CPU_VEC_EMU_IRQ + 1] << 8; 
+            cpu->cycles += 7;
+        }
+        else
+        {
+            cpu->PC = mem[CPU_VEC_NATIVE_IRQ];
+            cpu->PC |= mem[CPU_VEC_NATIVE_IRQ + 1] << 8;
+            cpu->PBR = 0;
+            cpu->cycles += 8;
+        }
+
+        cpu->P.D = 0; // Binary mode (65C02)
+        cpu->P.I = 1;
+
         return;
     }
 }
@@ -59,9 +226,19 @@ void stepCPU(CPU *cpu, int16_t *mem)
  * @param mem The memory array which is to be connected to the CPU
  * @param byte The byte to be pushed onto the stack
  */
-static void stackCPU_pushByte(CPU *cpu, int16_t *mem, int32_t byte)
+static void _stackCPU_pushByte(CPU *cpu, int16_t *mem, int32_t byte)
 {
-    mem[cpu->SP--] = byte;
+    mem[cpu->SP] = byte;
+
+    // Correct SP if in emulation mode
+    if ( cpu->P.E && (cpu->SP & 0xff == 0) )
+    {
+        cpu->SP = 0x01ff;
+    }
+    else
+    {
+        cpu->SP -= 1;
+    }
 }
 
 /**
@@ -70,10 +247,10 @@ static void stackCPU_pushByte(CPU *cpu, int16_t *mem, int32_t byte)
  * @param mem The memory array which is to be connected to the CPU
  * @param word The word to be pushed onto the stack
  */
-static void stackCPU_pushWord(CPU *cpu, int16_t *mem, int32_t word)
+static void _stackCPU_pushWord(CPU *cpu, int16_t *mem, int32_t word)
 {
-    mem[cpu->SP--] = (word & 0xff00) >> 8;
-    mem[cpu->SP--] = word & 0xff;
+    _stackCPU_pushByte(cpu, mem, (word & 0xff00) >> 8);
+    _stackCPU_pushByte(cpu, mem, word & 0xff);
 }
 
 /**
@@ -82,7 +259,7 @@ static void stackCPU_pushWord(CPU *cpu, int16_t *mem, int32_t word)
  * @param mem The memory array which is to be connected to the CPU
  * @return The value popped off the stack
  */
-static int32_t stackCPU_popByte(CPU *cpu, int16_t *mem)
+static int32_t _stackCPU_popByte(CPU *cpu, int16_t *mem)
 {
 
 }
@@ -93,7 +270,7 @@ static int32_t stackCPU_popByte(CPU *cpu, int16_t *mem)
  * @param mem The memory array which is to be connected to the CPU
  * @return The value popped off the stack
  */
-static int32_t stackCPU_popWord(CPU *cpu, int16_t *mem)
+static int32_t _stackCPU_popWord(CPU *cpu, int16_t *mem)
 {
 
 }
--- src/65816.h
+++ src/65816.h
@@ -35,7 +35,7 @@ struct CPU
         unsigned char Z : 1;
         unsigned char I : 1;
         unsigned char D : 1;
-        unsigned char X : 1;
+        unsigned char X : 1; // B in emulation
         unsigned char M : 1;
         unsigned char V : 1;
         unsigned char N : 1;
@@ -45,14 +45,21 @@ struct CPU
         unsigned char NMI : 1; // 1 if NMI input is asserted, 0 else
         // unsigned char ABT : 1; // 1 if ABORT input is asserted, 0 else
     } P;
+    uint32_t cycles; // Total phi-1 cycles the CPU has run
 };
+
+// Get the value of a CPU's SR
+// NOTE: __CPU__ must be a pointer to a CPU struct
+#define CPU_GET_SR(__CPU__) (__CPU__->P.C | (__CPU__->P.Z << 1) | (__CPU__->P.I << 2) | \
+                             (__CPU__->P.D << 3) | (__CPU__->P.X << 4) | (__CPU__->P.M << 5) | \
+                             (__CPU__->P.V << 6) | (__CPU__->P.N << 7))
 
 void resetCPU(CPU*);
 void stepCPU(CPU*, int16_t*);
-static void stackCPU_pushByte(CPU *, int16_t *mem, int32_t);
-static void stackCPU_pushWord(CPU *, int16_t *mem, int32_t);
-static int32_t stackCPU_popByte(CPU *, int16_t *mem);
-static int32_t stackCPU_popWord(CPU *, int16_t *mem);
+static void _stackCPU_pushByte(CPU *, int16_t *mem, int32_t);
+static void _stackCPU_pushWord(CPU *, int16_t *mem, int32_t);
+static int32_t _stackCPU_popByte(CPU *, int16_t *mem);
+static int32_t _stackCPU_popWord(CPU *, int16_t *mem);
 
 
 #endif
