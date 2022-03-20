0f55118f3b0a9fb0da8acbf71b3cb1298a136629
Electronics Tinkerer <electronicstinkerer@gmail.com>
2022-03-20 00:12:07 -0400

    Update _stackCPU_pushByte

--- src/65816-ops.h
+++ src/65816-ops.h
@@ -4,27 +4,4 @@
 
 #include "65816.h"
 
-static void _stackCPU_pushByte(CPU_t *, memory_t *, int32_t);
-static void _stackCPU_pushWord(CPU_t *, memory_t *, int32_t, Emul_Stack_Mod_t);
-static void _stackCPU_push24(CPU_t *, memory_t *, int32_t);
-static int32_t _stackCPU_popByte(CPU_t *, memory_t *, Emul_Stack_Mod_t);
-static int32_t _stackCPU_popWord(CPU_t *, memory_t *, Emul_Stack_Mod_t);
-static int32_t _stackCPU_pop24(CPU_t *, memory_t *);
-
-static int32_t _addrCPU_getAbsoluteIndexedIndirectX(CPU_t *, memory_t *);
-static int32_t _addrCPU_getAbsoluteIndirect(CPU_t *, memory_t *);
-static int32_t _addrCPU_getAbsoluteIndirectLong(CPU_t *, memory_t *);
-static int32_t _addrCPU_getAbsolute(CPU_t *, memory_t *);
-static int32_t _addrCPU_getAbsoluteIndexedX(CPU_t *, memory_t *);
-static int32_t _addrCPU_getAbsoluteIndexedY(CPU_t *, memory_t *);
-static int32_t _addrCPU_getLongIndexedX(CPU_t *, memory_t *);
-static int32_t _addrCPU_getDirectPage(CPU_t *, memory_t *);
-static int32_t _addrCPU_getDirectPageIndexedX(CPU_t *, memory_t *);
-static int32_t _addrCPU_getDirectPageIndexedY(CPU_t *, memory_t *);
-static int32_t _addrCPU_getDirectPageIndirect(CPU_t *, memory_t *);
-static int32_t _addrCPU_getDirectPageIndirectLong(CPU_t *, memory_t *);
-static int32_t _addrCPU_getRelative8(CPU_t *, memory_t *);
-static int32_t _addrCPU_getRelative16(CPU_t *, memory_t *);
-static int32_t _addrCPU_getLong(CPU_t *, memory_t *);
-
 #endif
--- src/65816-util.h
+++ src/65816-util.h
@@ -6,9 +6,11 @@
 #include <stdint.h>
 #include "65816.h"
 
-// CPU-related functions
+// CPU-related helper functions
+void _cpu_update_pc(CPU_t *cpu, uint16_t offset);
 uint8_t _cpu_get_sr(CPU_t *);
 void _cpu_set_sr(CPU_t *, uint8_t);
+void _cpu_set_sp(CPU_t *, uint16_t);
 uint32_t _cpu_get_pbr(CPU_t *);
 uint32_t _cpu_get_effective_pc(CPU_t *);
 void _cpu_update_pc(CPU_t *, uint16_t);
@@ -23,5 +25,30 @@ uint16_t _get_mem_word_bank_wrap(memory_t *, uint32_t);
 // access data within the memory_t datastructure
 uint8_t _get_mem_byte(memory_t *, uint32_t);
 uint16_t _get_mem_word(memory_t *, uint32_t);
+void _get_mem_byte(memory_t *mem, uint32_t addr, uint8_t val);
+
+// CPU-Addressing Modes
+static void _stackCPU_pushByte(CPU_t *, memory_t *, int8_t);
+static void _stackCPU_pushWord(CPU_t *, memory_t *, uint16_t, Emul_Stack_Mod_t); // TODO
+static void _stackCPU_push24(CPU_t *, memory_t *, int32_t);                      // TODO
+static int32_t _stackCPU_popByte(CPU_t *, memory_t *, Emul_Stack_Mod_t);         // TODO
+static int32_t _stackCPU_popWord(CPU_t *, memory_t *, Emul_Stack_Mod_t);         // TODO
+static int32_t _stackCPU_pop24(CPU_t *, memory_t *);                             // TODO
+
+static int32_t _addrCPU_getAbsoluteIndexedIndirectX(CPU_t *, memory_t *); // TODO
+static int32_t _addrCPU_getAbsoluteIndirect(CPU_t *, memory_t *);         // TODO
+static int32_t _addrCPU_getAbsoluteIndirectLong(CPU_t *, memory_t *);     // TODO
+static int32_t _addrCPU_getAbsolute(CPU_t *, memory_t *);                 // TODO
+static int32_t _addrCPU_getAbsoluteIndexedX(CPU_t *, memory_t *);         // TODO
+static int32_t _addrCPU_getAbsoluteIndexedY(CPU_t *, memory_t *);         // TODO
+static int32_t _addrCPU_getLongIndexedX(CPU_t *, memory_t *);             // TODO
+static int32_t _addrCPU_getDirectPage(CPU_t *, memory_t *);               // TODO
+static int32_t _addrCPU_getDirectPageIndexedX(CPU_t *, memory_t *);       // TODO
+static int32_t _addrCPU_getDirectPageIndexedY(CPU_t *, memory_t *);       // TODO
+static int32_t _addrCPU_getDirectPageIndirect(CPU_t *, memory_t *);       // TODO
+static int32_t _addrCPU_getDirectPageIndirectLong(CPU_t *, memory_t *);   // TODO
+static int32_t _addrCPU_getRelative8(CPU_t *, memory_t *);                // TODO
+static int32_t _addrCPU_getRelative16(CPU_t *, memory_t *);               // TODO
+static int32_t _addrCPU_getLong(CPU_t *, memory_t *);                     // TODO
 
 #endif
