d00d1de89f4d0edcbb6de326f78941d8a04abe87
Electronics Tinkerer <electronicstinkerer@gmail.com>
2023-01-08 14:02:38 -0500

    Add window borders

--- src/debugger.c
+++ src/debugger.c
@@ -36,35 +36,33 @@ void print_header(int width, state_t sim_state, status_t status_id)
  * Print the current state of the CPU's registers at the 
  * location that the cursor currently is on screen
  * 
+ * @param *win The curses WINDOW to print the display in
  * @param *cpu The CPU to print
  * @param y The upper left corner of the data display
  * @param x The upper left corner of the data display
  */
-void print_cpu_regs(CPU_t *cpu, int y, int x)
+void print_cpu_regs(WINDOW *win, CPU_t *cpu, int y, int x)
 {
     // Data headers/labels
-    attrset(A_NORMAL);
-    mvprintw(y, x, "C:   X:   Y:   SP:");
-    mvprintw(y+3, x, "DBR: PBR: PC:");
-    mvprintw(y+6, x, "RST: IRQ: NMI: STP: CRASH:");
-    mvprintw(y, x+22, "NVMXDIZC|E");
-    mvprintw(y+3, x+22, "Cycles:");
+    wattrset(win, A_NORMAL);
+    mvwprintw(win, y, x, "C:   X:   Y:   SP:");
+    mvwprintw(win, y+3, x, "DBR: PBR: PC:");
+    mvwprintw(win, y+6, x, "RST: IRQ: NMI: STP: CRASH:");
+    mvwprintw(win, y, x+22, "NVMXDIZC|E");
+    mvwprintw(win, y+3, x+22, "Cycles:");
 
     // Data itself
-    attron(A_BOLD);
-    mvprintw(y+1, x, "%04x %04x %04x %04x",
+    wattron(win, A_BOLD);
+    mvwprintw(win, y+1, x, "%04x %04x %04x %04x",
              cpu->C, cpu->X, cpu->Y, cpu->SP);
-    mvprintw(y+4, x, "%02x   %02x   %04x",
+    mvwprintw(win, y+4, x, "%02x   %02x   %04x",
              cpu->DBR, cpu->PBR, cpu->PC);
-    mvprintw(y+7, x, "%d    %d    %d    %d    %d",
+    mvwprintw(win, y+7, x, "%d    %d    %d    %d    %d",
              cpu->P.RST, cpu->P.IRQ, cpu->P.NMI, cpu->P.STP, cpu->P.CRASH);
-    mvprintw(y+1, x+22, "%d%d%d%d%d%d%d%d|%d",
+    mvwprintw(win, y+1, x+22, "%d%d%d%d%d%d%d%d|%d",
              cpu->P.N, cpu->P.V, cpu->P.M, cpu->P.XB, cpu->P.D,
              cpu->P.I, cpu->P.Z, cpu->P.C, cpu->P.E);
-    mvprintw(y+4, x+22, "%010ld", cpu->cycles);
-             
-    attrset(A_NORMAL);
-
+    mvwprintw(win, y+4, x+22, "%010ld", cpu->cycles);
 }
 
 
@@ -74,6 +72,7 @@ int main(int argc, char *argv[])
     int scrw, scrh;         // Width and height of screen
     state_t sim_state = STATE_IDLE;
     status_t status_id = STATUS_NONE;
+    WINDOW *win_watch1, *win_watch2, *win_cpu, *win_cmd, *win_inst_hist;
     
     CPU_t cpu;
     resetCPU(&cpu);
@@ -93,6 +92,13 @@ int main(int argc, char *argv[])
     keypad(stdscr, TRUE);   // Enable handling of function and other special keys
     noecho();               // Disable echoing of user-typed characters
 
+    // Set up each window (height, width, starty, startx)
+    win_watch1 = newwin(scrh/2, scrw/2, 1, 0);
+    win_watch2 = newwin(scrh/2, scrw/2, scrh/2, 0);
+    win_cpu = newwin(10, scrw/2, 1, scrw/2);
+    win_cmd = newwin(3, scrw/2, scrh-3, scrw/2);
+    win_inst_hist = newwin(scrh - 10 - 3 + 1, scrw/2, 10, scrw/2);
+
     // Event loop
     prev_c = c = EOF;
     // F12 F12 = exit
@@ -105,11 +111,9 @@ int main(int argc, char *argv[])
         // case KEY_F(6): // Run until breakpoint
         case KEY_F(7): // Step
             stepCPU(&cpu, memory);
-            printw("F7");
             break;
         case KEY_F(9):
             resetCPU(&cpu);
-            printw("F9");
             break;
         default:
             // printw("Default '%d' \"%d\" '%d' %d", c, KEY_F(12), prev_c, KEY_F(12) == prev_c);
@@ -126,9 +130,23 @@ int main(int argc, char *argv[])
         }
 
         // Update screen
-        getmaxyx(stdscr, scrh, scrw); // Get screen dimensions
+        // getmaxyx(stdscr, scrh, scrw); // Get screen dimensions
         print_header(scrw, sim_state, status_id);
-        print_cpu_regs(&cpu, 2, scrw/2);
+        print_cpu_regs(win_cpu, &cpu, 1, 2);
+
+        // Window borders
+        wborder(win_watch1, '|', '|', '=', '=', '+', '+', '+', '+');
+        wborder(win_watch2, '|', '|', '=', '=', '+', '+', '+', '+');
+        wborder(win_cpu, '|', '|', '=', '=', '+', '+', '+', '+');
+        wborder(win_cmd, '|', '|', '=', '=', '+', '+', '+', '+');
+        wborder(win_inst_hist, '|', '|', '=', '=', '+', '+', '+', '+');
+
+        // Order of refresh matters - layering of title bars
+        wrefresh(win_cpu);
+        wrefresh(win_inst_hist);
+        wrefresh(win_cmd);
+        wrefresh(win_watch1);
+        wrefresh(win_watch2);
         refresh();
         status_id = STATUS_NONE;
         prev_c = c;
@@ -138,6 +156,11 @@ int main(int argc, char *argv[])
 
     printw("Hello World !!!");	/* Print Hello World		  */
 
+    delwin(win_watch1);
+    delwin(win_watch2);
+    delwin(win_cpu);
+    delwin(win_cmd);
+    delwin(win_inst_hist);
     endwin();			// Clean up curses mode
     return 0;
 }
