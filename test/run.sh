8278bd1b382e61267fea619c712b5675a1438b71
Electronics Tinkerer <electronicstinkerer@gmail.com>
2023-06-17 17:36:01 -0400

    [test] Parallelize processor test runs

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,33 @@
-#!/bin/bash -e
-if [ ! -f output.scpu ]; then
-    ./json2cpu.py ~/ProcessorTests/65816/v1/*
-fi
-cc -g runner.c ../src/cpu/*.c && ./a.out output.scpu
+#!/bin/bash
+cc runner.c ../src/cpu/*.c
 
+TEST_DIR="op_tests"
+RSLT_DIR="results"
+
+if [ ! -d "$TEST_DIR" ] ; then
+    mkdir -p "$TEST_DIR"
+fi
+if [ ! -d "$RSLT_DIR" ] ; then
+    mkdir -p "$RSLT_DIR"
+fi
+
+
+run_test() {
+    echo "$1"
+    TEST_FILE="$TEST_DIR/$1.scpu"
+    RSLT_FILE="$RSLT_DIR/$1.out"
+
+    if [ ! -f "$TEST_FILE" ]; then
+        ./json2cpu.py "$TEST_FILE" ~/ProcessorTests/65816/v1/"$1".{e,n}.json
+    fi
+
+    ./a.out "$TEST_FILE" > "$RSLT_FILE"
+}
+
+for i in {0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f}{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f}; do
+    run_test "$i" &
+done
+
+wait
+
+echo "Complete"
